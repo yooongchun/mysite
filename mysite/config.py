ca60b7a049484e18123c0e17841e1d276febe3af
zhayongchun <yooongchun@foxmail.com>
2019-06-09 09:29:53 +0800

    add config file:without secret info.

---
+++ mysite/config.py
@@ -0,0 +1,9 @@
+# 配置全局参数
+DEBUG_MODE = False  # 调试模式：在本地测试时使用True
+# Mysql数据库连接信息
+MYSQL_PARAS = {
+    'NAME': 'blog',
+    'USER': 'zhayongchun',
+    'PASSWORD': '***',
+    'HOST': '127.0.0.1',
+    'PORT': '3306', }
